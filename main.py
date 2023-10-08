615bb545167b5fc11978a0bc6ad328c712182399
Khai Do <khaitroyy@gmail.com>
2023-10-08 01:11:58 -0500

    add send sms function

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import requests
+import os
+from twilio.rest import Client
+
 
 MY_LAT = 29.760427
 MY_LON = -95.369804
 api_key = "7622173cb7e5115a206998e2e5d86421"
 
+TWILIO_ACCOUNT_SID = "AC8d6e81afbcaba7e94b4f1834dafb84de"
+TWILIO_AUTH_TOKEN = "7b79ff8eaab747e5d2c60fdfa2d8303f"
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+#1----------SMS Sending----------------------
+def send_sms():
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+    message = client.messages \
+                    .create(
+                        body="Ngan oi, It's going to rain today. Remember to bring an umberlla nha. ",
+                        from_='+18556430691',
+                        to='+8322432370'
+                    )
+
+    print(message.sid)
+    
 weather_params = {
     "lat": MY_LAT,
     "lon": MY_LON,
@@ -25,4 +47,5 @@ for item in hourly_data:
         will_rain = True
 
 if will_rain:
-    print("Bring un Umbrella. ")
+    send_sms()
+
