4ee114ac7bd2c414fa481312e36fc2bb6d739ff6
Khai Do <khaitroyy@gmail.com>
2023-10-08 01:25:42 -0500

    add api

--- main.py
+++ main.py
@@ -2,29 +2,27 @@ import requests
 import os
 from twilio.rest import Client
 
-
 MY_LAT = 29.760427
 MY_LON = -95.369804
 api_key = "7622173cb7e5115a206998e2e5d86421"
 
-TWILIO_ACCOUNT_SID = "AC8d6e81afbcaba7e94b4f1834dafb84de"
-TWILIO_AUTH_TOKEN = "7b79ff8eaab747e5d2c60fdfa2d8303f"
+account_sid = "AC8d6e81afbcaba7e94b4f1834dafb84de"
+auth_token = "a956b69e108d6dc121b8b1142f72f695"
 
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+# account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+# auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 
 #1----------SMS Sending----------------------
 def send_sms():
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-
+    client = Client(account_sid, auth_token)
     message = client.messages \
                     .create(
-                        body="Ngan oi, It's going to rain today. Remember to bring an umberlla nha. ",
+                        body="Hello, it's going to rain to day. ",
                         from_='+18556430691',
-                        to='+8322432370'
+                        to='+18322432370'
                     )
 
-    print(message.sid)
+    print(message.status)
     
 weather_params = {
     "lat": MY_LAT,
@@ -37,13 +35,13 @@ OWN_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
 
 will_rain = False 
 
-respone = requests.get(OWN_Endpoint, params=weather_params)
-respone.raise_for_status()
-data = respone.json()
+response = requests.get(OWN_Endpoint, params=weather_params)
+response.raise_for_status()
+data = response.json()
 hourly_data = data["hourly"][0:13]
 for item in hourly_data:
     weather_id = hourly_data[0]["weather"][0]["id"]
-    if int(weather_id) < 700:
+    if int(weather_id) > 700:
         will_rain = True
 
 if will_rain:
